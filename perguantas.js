143f40146d63d908ffe157b2a017bb363741b4b1
Mariacanedo <140858354+Mariacanedo@users.noreply.github.com>
2024-10-09 21:48:00 -0300

    aa

---
+++ perguantas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'alunos',
+    'qual é o insta da aluna mais bonita?',
+    ' @Mariacanedoo!'
+)
+
+criaCartao(
+    'Feriado',
+    'qual o dia é considerado diadas criança?',
+    'Dia 12 de outubro '
+)
+
+criaCartao(
+    'professor',
+    'qual o melhor professor do cerb',
+    'Rafinha querido'
+)
+
+criaCartao(
+    'Ganhando o Mundo',
+    'Qual a melhor estudante que ja participou do Programa Ganhado o mundo no cerb?',
+    'Isabella Parra Hass👏👏👏'
+)
